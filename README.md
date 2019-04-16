682392619730d8c48531bee9c8907a251845508b
Simon Bäumer <simon.baeumer@sva.de>
2019-04-16 09:35:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+First commit on repo01
---
+++ sync.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+REMOTE="git@github.com:SimonBaeumer/"
+BRANCH="master"
+
+for i in $(ls repos/); do
+    SHA1=$(splitsh-lite --prefix=repos/repo01)
+
+    git checkout "${SHA1}"
+    git checkout -b "${i}"
+
+    git remote add "${i}" "${REMOTE}"/"${i}"
+    git push "${i}" "${i}":"${BRANCH}" --tags
+
+    git checkout "${BRANCH}"
+    git branch -D "${i}"
+    git remote rm "${i}"
+done
+
+
+
